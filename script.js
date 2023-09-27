ea5640fb7a2ca7340ec2000b06bc0341a4557850
Arjun Maniyani <arjunrmaniyani@gmail.com>
2023-09-27 10:06:24 +0530

    uploaded

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "~";
+const apiKey = "bcb5b5ee884b1b5ce803e26025323053";
 const apiUrl = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 function checkPrefix( weatherId,  pre)
@@ -23,30 +23,30 @@ async function checkWeather(city){
 
     var weatherId=data.weather[0].id;
     if(checkPrefix(weatherId, 2)){
-        document.getElementById("weather-icon").src = "/weather-icons/cloud-lightning.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/cloud-lightning.svg";
     }
     else if(checkPrefix(weatherId, 3)){
-        document.getElementById("weather-icon").src = "/weather-icons/cloud-rain.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/cloud-rain.svg";
     }
     else if(checkPrefix(weatherId, 5)){
-        document.getElementById("weather-icon").src = "/weather-icons/cloud-rain.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/cloud-rain.svg";
     }
     else if(checkPrefix(weatherId, 6)){
-        document.getElementById("weather-icon").src = "/weather-icons/cloud-snow.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/cloud-snow.svg";
     }
     else if(checkPrefix(weatherId, 7)){
-        document.getElementById("weather-icon").src = "/weather-icons/cloud-fog.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/cloud-fog.svg";
     }
     else if(checkPrefix(weatherId, 800)){
-        document.getElementById("weather-icon").src = "/weather-icons/sun.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/sun.svg";
     }
     else if(checkPrefix(weatherId, 80)){
-        document.getElementById("weather-icon").src = "/weather-icons/cloud-sun.svg";
+        document.getElementById("weather-icon").src = "./weather-icons/cloud-sun.svg";
     }
     
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
-    document.querySelector(".humidity").innerHTML = "Humidity: " + data.main.humidity+"%";
+    document.querySelector(".humidity").innerHTML = "Humidity: \n" + data.main.humidity+"%";
     document.querySelector(".desc").innerHTML = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
     document.querySelector(".wind").innerHTML = "Wind: "+data.wind.speed+" km/h";
     document.querySelector(".city").innerHTML = data.name;
